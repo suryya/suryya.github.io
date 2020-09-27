f486cf8d2028944da1394d8f5f6f092a7f5add4f
Surya <suryya.gh@gmail.com>
2020-09-28 01:23:30 +0530

    Build files added

---
+++ precache-manifest.c0a79e8158d68702c352bb739bae8c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62f1240a28c11c920a6fc198b3f6291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e0e76869f93cae1a571",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3a309904b6806b1895ce",
+    "url": "/static/js/2.3af9eed0.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.3af9eed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e0e76869f93cae1a571",
+    "url": "/static/js/main.51ccbab5.chunk.js"
+  },
+  {
+    "revision": "143f365fbedbdfcb3e60",
+    "url": "/static/js/runtime-main.dcf8ae98.js"
+  }
+]);
